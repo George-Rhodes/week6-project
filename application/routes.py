351ce35af4aac1263a933a3408b0cdfe7c801c68
George-Rhodes <george_rhodes@hotmail.co.uk>
2020-11-12 12:23:00 +0000

    removed old variable names

--- application/routes.py
+++ application/routes.py
@@ -22,7 +22,7 @@ def index():
 def add():
 	form = routeForm()
 	if form.validate_on_submit():
-		new_routine= Routine(task=form.task.data)
+		new_routine= Routine(rTitle=form.rTitle.data, auhtor=form.author.data, description)
 		db.session.add(new_routine)
 		db.session.commit()
 		return redirect(url_for('index'))
