9378a7a6d0728e0d6da9b0554b6115105702f6ca
George-Rhodes <george_rhodes@hotmail.co.uk>
2020-11-13 12:39:00 +0000

    adding excersise to routine

--- application/models.py
+++ application/models.py
@@ -6,14 +6,14 @@ class Routine(db.Model):
     rTitle = db.Column(db.String(30), nullable=False)
     author = db.Column(db.String(30), nullable = False)
     description = db.Column(db.String(255), nullable=False)  
-    routine = db.relationship('Excer', backref='routine')
+    routine = db.relationship('Excer', backref='routine', cascade="all, delete")
 
     
 
 
 class Excer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    routine_id = db.Column(db.Integer, db.ForeignKey('routine.id'), nullable=False)
+    routine_id = db.Column(db.Integer, db.ForeignKey('routine.id'), nullable=False, ondelete="CASCADE")
     set_name = db.Column(db.String(50), nullable=False)
     level_num = db.Column(db.Integer, nullable = False)
     level_type = db.Column(db.String(10), nullable = False)
